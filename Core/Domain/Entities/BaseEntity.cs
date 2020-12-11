bef51fa69777d4f992c918a3bcf824427163cbfb
Pablo Groeler <pablo_groeler@hotmail.com>
2020-12-11 17:13:43 -0400

    Initial commit

---
+++ Core/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace test_deliverit.Core.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id {get; set;}
+    }
+}
---
+++ Core/Domain/Entities/ContaPagar.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace test_deliverit.Core.Domain.Entities
+{
+    public class ContaPagar: BaseEntity
+    {
+        public string Nome { get; set; }
+        public decimal ValorOriginal { get; set; }
+        public decimal ValorCorrigido { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public int DiasAtraso { get; set; }
+        public decimal PercentualMulta { get; set; }
+        public decimal PercentualJuroDia { get; set; }
+
+    }
+}
